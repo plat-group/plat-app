e422d32d1344c2c46bc0e5dc3447b9c20fc5b3ac
Son Truong <sontt@vaixgroup.com>
2021-12-03 23:26:54 +0700

    Add command Near CLI

---
+++ app/Listeners/PayCoinListener.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PlayedGameEvent;
+use App\Services\CampaignService;
+use Illuminate\Console\Application;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Symfony\Component\Process\Process;
+
+class PayCoinListener implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * @var \App\Services\CampaignService
+     */
+    protected $campaignService;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(CampaignService $campaignService)
+    {
+        $this->campaignService = $campaignService;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PlayedGameEvent  $event
+     * @return void
+     */
+    public function handle(PlayedGameEvent $event)
+    {
+        $campaign = $this->campaignService->find($event->campaignId);
+
+        $command = 'near call platserver.testnet withdraw \''
+                   . $this->makeParameters($campaign) . '\''
+                   . '--accountId platserver.testnet';
+
+        $this->commandlineRun($command);
+    }
+
+    /**
+     * Run a commandline
+     *
+     * @param $command
+     *
+     * @return bool
+     */
+    public function commandlineRun($command)
+    {
+        $command = Application::formatCommandString($command) . ' ' . ' > /dev/null 2>&1 &';
+
+        Process::fromShellCommandline($command, base_path())->run();
+
+        return true;
+    }
+
+    /**
+     * Create parameters of Near CLI command
+     *
+     * @param $campaign
+     *
+     * @return false|string
+     */
+    private function makeParameters($campaign)
+    {
+        $result = [
+            'client' => 'nghilt.testnet',
+            'user_id' => 'platuser.testnet',
+            'referral_id' => 'platreferral.testnet',
+            'creator_id' => 'platcreator.testnet',
+            'team' => 'platteam.testnet',
+            'amount_user' => $this->toYokto($campaign->user_budget),
+            'amount_referral' => $this->toYokto($campaign->referral_budget),
+            'amount_creator' => $this->toYokto($campaign->creator_budget),
+            'amount_team' => $this->toYokto(1),
+        ];
+
+        return json_encode($result);
+    }
+
+    /**
+     * Convert to Yokto for amount withdraw by Near CLI
+     *
+     * @param float|int $number
+     *
+     * @return float
+     */
+    private function toYokto($number)
+    {
+        return $number * 1000000000000000000000000 ;
+    }
+}
