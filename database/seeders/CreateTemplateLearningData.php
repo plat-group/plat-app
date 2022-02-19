40ff1acb22ce19d12f279c5715bb1cab6b8344e7
vaix-nghi <nghilt@vaixgroup.com>
2022-02-19 15:28:00 +0900

    Create learning data

--- database/seeders/CreateTemplateLearningData.php
+++ database/seeders/CreateTemplateLearningData.php
@@ -8,6 +8,10 @@ use App\Models\Game;
 use App\Models\GameTemplate;
 use App\Models\Order;
 use App\Models\User;
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\Question;
+use Illuminate\Support\Facades\DB;
 
 class CreateTemplateLearningData extends Seeder
 {
@@ -25,9 +29,14 @@ class CreateTemplateLearningData extends Seeder
         $this->createOrderData();
         $this->createCampaignData();
         $this->createTransactionData();
+
+        $this->createCourseData();
+        $this->createLessonData();
+        $this->createQuestionAndAnswerData();
     }
 
-    private function createUserData() {
+    private function createUserData()
+    {
         $user = new User();
         $user->name = 'PlatCreator';
         $user->email = 'creator@plats.network';
@@ -89,10 +98,241 @@ class CreateTemplateLearningData extends Seeder
         $user->balance = 0;
         $user->blocked_balance = 0;
         $user->save();
+    }
+
+    private function createCourseData()
+    {
+        $clientId = User::where('email', 'client@plats.network')->pluck('id')->first();
+
+        $course = new Course();
+
+        $course->id = 'near-learning-course-1';
+        $course->creator_id = $clientId;
+        $course->name = 'Basic NEAR Protocol learning course';
+        $course->description = 'Reimagine finance, creativity, and community with NEAR';
+        $course->thumbnail = 'l2e/image/basic_near_course.png';
+        $course->save();
+    }
+
+    private function createLessonData()
+    {
+        $courseId = Course::pluck('id')->first();
+
+        $lesson = new Lesson();
+        $lesson->course_id = $courseId;
+        $lesson->id = 'basic-near-lesson-1';
+        $lesson->name = 'What is NEAR?';
+        $lesson->description = 'NEAR is a layer-1 blockchain that is simple to use, super fast, and incredibly secure. NEAR has been awarded the Climate Neutral Product Label from the South Pole and is actively helping users and developers reimagine finance, community, and creativity. Get to grips with what NEAR is, how it works, and discover why it’s so powerful for developers and users.';
+        $lesson->thumb = 'l2e/image/1-What-is-NEAR.png';
+        $lesson->content_url = 'l2e/video/what-is-near.mp4';
+        $lesson->content_type = 0;
+        $lesson->save();
+
+        $lesson = new Lesson();
+        $lesson->course_id = $courseId;
+        $lesson->id = 'basic-near-lesson-2';
+        $lesson->name = 'NEAR Wallet & Staking';
+        $lesson->description = 'NEAR wallet is a non-custodial, web-based application, that gives you full control over your assets — allowing you to create, play, and stake without restrictions. Sign-up takes just a few clicks, and once complete the world of staking rewards, NFTs, and more will be accessible and easy to use.';
+        $lesson->thumb = 'l2e/image/2-Wallet.png';
+        $lesson->content_url = 'l2e/video/NEAR-Wallet-Staking.mp4';
+        $lesson->content_type = 0;
+        $lesson->save();
+
+        $lesson = new Lesson();
+        $lesson->id = 'basic-near-lesson-3';
+        $lesson->course_id = $courseId;
+        $lesson->name = 'NEAR Sharding in a Nutshell';
+        $lesson->description = "NEAR’s blockchain uses a revolutionary new type of sharding tool called Nightshade to help create infinite scalability and speed. In this lesson, we explore how it works and why it makes sure the NEAR network is primed for mass adoption.";
+        $lesson->thumb = 'l2e/image/3-Sharding.png';
+        $lesson->content_url = 'l2e/video/xxx';
+        $lesson->content_type = 0;
+        $lesson->save();
+
+        $lesson = new Lesson();
+        $lesson->id = 'basic-near-lesson-4';
+        $lesson->course_id = $courseId;
+        $lesson->name = 'The Rainbow Bridge';
+        $lesson->description = 'Rainbow Bridge is a gateway to other blockchains. The Bridge allows users to seamlessly connect NEAR with other blockchains allowing the movement of tokens into new ecosystems with little more than a few clicks. As a trustless, permissionless tool for cross-chain transfers, it’s the easiest, and simplest way to move assets between NEAR and Ethereum.';
+        $lesson->thumb = 'l2e/image/4-Rainbow-Bridge.png';
+        $lesson->content_url = 'l2e/video/xxx';
+        $lesson->content_type = 0;
+        $lesson->save();
+
+        $lesson = new Lesson();
+        $lesson->id = 'basic-near-lesson-5';
+        $lesson->course_id = $courseId;
+        $lesson->name = 'What is Aurora?';
+        $lesson->description = 'Aurora allows Ethereum\'s most popular applications to leverage NEAR\'s powerful blockchain protocol to dramatically increase scalability and efficiency while cutting fees by up to 99.99%.';
+        $lesson->thumb = 'l2e/image/5-Aurora.png';
+        $lesson->content_url = 'l2e/video/xxx';
+        $lesson->content_type = 0;
+        $lesson->save();
+    }
+
+    private function createQuestionAndAnswerData()
+    {
+        $lessonDatas = $this->getLessonData();
+        foreach($lessonDatas as $lessonId => $questions) {
+            // loop question
+            foreach($questions as $idx => $questionObj) {
+                // Register question
+                $questionData = $questionObj['question'];
+                $question = new Question();
+                $question->lesson_id = $lessonId;
+                $question->id = $lessonId . '-question-' . ($idx + 1);
+                $question->question_point = $questionData['point'];
+                $question->question = $questionData['question'];
+                $question->save();
+
+                // Register answer
+                $answers = $questionObj['answers'];
+                $this->registerLessonAnswer($lessonId, $answers);
+            }
+        }
+
+        // $lesson1Id = Lesson::where('id', 'basic-near-lesson-1')->pluck('id')->first();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-1';
+        // $question->question_point = '18';
+        // $question->question = 'What technology does NEAR use to increase number of transactions and scalability?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-2';
+        // $question->question_point = '40';
+        // $question->question = 'Who is the founder NEAR protocol?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-3';
+        // $question->question_point = '49';
+        // $question->question = 'Which consensus mechanism does NEAR implement?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-4';
+        // $question->question_point = '61';
+        // $question->question = 'Which role ensures network security?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-5';
+        // $question->question_point = '77';
+        // $question->question = 'What do validators do in NEAR?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-6';
+        // $question->question_point = '85';
+        // $question->question = 'What happens if validators is malacious?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-7';
+        // $question->question_point = '89';
+        // $question->question = 'Can normal users staking or not?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-8';
+        // $question->question_point = '91';
+        // $question->question = 'How developer in Ethereum can develop Dapps in NEAR?';
+        // $question->save();
+
+        // $question = new Question();
+        // $question->lesson_id = $lesson1Id;
+        // $question->id = 'basic-near-lesson-1-question-9';
+        // $question->question_point = '138';
+        // $question->question = 'How to register near mainnet wallet?';
+        // $question->save();
+
 
     }
 
-    private function createGameTemplateData() {
+    private function registerLessonAnswer($lessonId, $answers)
+    {
+        foreach ($answers as $answer => $isCorrect) {
+            DB::table('lesson_answers')->save([
+                'question_id' => $lessonId,
+                'answer' => $answer,
+                'correct' => $isCorrect
+            ]);
+        }
+    }
+
+    private function getLessonData()
+    {
+        return [
+            'basic-near-lesson-1' => [
+                [
+                    'question' => [
+                        'point' => 18, 'text' => 'What technology does NEAR use to increase number of transactions and scalability?',
+                    ],
+                    'answers' => ['Sharding' => true, 'Side-chain' => false, 'Hub' => false]
+                ],
+                [
+                    'question' => [
+                        'point' => 40, 'text' => 'Who is the founder NEAR protocol?',
+                    ],
+                    'answers' => ['Alexander Skidanov' => false, 'Ilya Polosukhin' => false, 'Both A and B' => true],
+                ],
+                [
+                    'question' => [
+                        'point' => 49, 'text' => 'Which consensus mechanism does NEAR implement?',
+                    ],
+                    'answers' => ['Proof of Stake' => true, 'Proof of Work' => false, 'BFT' => false],
+                ],
+                [
+                    'question' => [
+                        'point' => 61, 'text' => 'Which role ensures network security?',
+                    ],
+                    'answers' => ['Validator' => true, 'Nominator' => false, 'Noone' => false],
+                ],
+                [
+                    'question' => [
+                        'point' => 62, 'text' => 'What do validators do in NEAR?',
+                    ],
+                    'answers' => ['Nothing' => false, 'Staking ' => true, 'Delegating' => false],
+                ],
+                [
+                    'question' => [
+                        'point' => 63, 'text' => 'What happens if validators is malacious?',
+                    ],
+                    'answers' => ['Có phần thưởng' => false, 'Mất hết lượng Staking' => false, 'Sẽ ko dc làm Validators nữa' => false, 'Both' => true],
+                ],
+                [
+                    'question' => [
+                        'point' => 67, 'text' => 'Can normal users staking or not?',
+                    ],
+                    'answers' => ['Có' => false, 'Không' => true, '' => false],
+                ],
+                [
+                    'question' => [
+                        'point' => 91, 'text' => 'How developer in Ethereum can develop Dapps in NEAR?',
+                    ],
+                    'answers' => ['Aurora' => false, 'Rainbow Bridge' => false, 'Both A and B' => true],
+                ],
+                [
+                    'question' => [
+                        'point' => 138, 'text' => 'How to register near mainnet wallet?',
+                    ],
+                    'answers' => ['<tên địa chỉ>.near' => true, '<tên địa chỉ>.testnet' => false, '<tên địa chỉ>.near-testnet' => false],
+                ]
+            ]
+        ];
+    }
+
+    private function createGameTemplateData()
+    {
         $creatorIds = User::where('role', CREATOR_ROLE)->pluck('id')->all();
 
         $creatorId = $this->getRandomCreator($creatorIds);
@@ -146,7 +386,8 @@ class CreateTemplateLearningData extends Seeder
         $game->save();
     }
 
-    private function createOrderData() {
+    private function createOrderData()
+    {
         $clientId = User::where('email', 'client@plats.network')->pluck('id')->first();
         $gameTemplateId = GameTemplate::pluck('id')->first();
         $gameId = Game::pluck('id')->first();
@@ -174,7 +415,8 @@ class CreateTemplateLearningData extends Seeder
         $order->save();
     }
 
-    private function createGameData() {
+    private function createGameData()
+    {
         $clientId = User::where('email', 'client@plats.network')->pluck('id')->first();
         // $gameTemplate = GameTemplate::where('name', 'Plats game car')->first();
 
@@ -201,7 +443,8 @@ class CreateTemplateLearningData extends Seeder
         $game->save();
     }
 
-    private function createCampaignData() {
+    private function createCampaignData()
+    {
         $gameId = Game::pluck('id')->first();
 
         $campaign = new Campaign();
@@ -217,11 +460,13 @@ class CreateTemplateLearningData extends Seeder
         $campaign->save();
     }
 
-    private function createTransactionData() {
+    private function createTransactionData()
+    {
         \App\Models\Transaction::factory(10)->create();
     }
 
-    private function getRandomCreator($creatorIds) {
+    private function getRandomCreator($creatorIds)
+    {
         $count = count($creatorIds);
         $idx = rand(0, $count - 1);
         return $creatorIds[$idx];
